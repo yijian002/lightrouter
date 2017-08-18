15027b33c6df60fbe50835df7ec70174a863bd4e
yijian002 <yijian002@gmail.com>
2017-08-18 14:12:37 +0800

    addEventListener hashchange

--- src/lightrouter.js
+++ src/lightrouter.js
@@ -13,324 +13,326 @@
  *  limitations under the License.
  */
 
-(function(window, factory) {
+(function(factory) {
 
-	if (typeof exports === 'object')
-	{
-		module.exports = factory(window);
-	}
-	else
-	{
-		window.LightRouter = factory(window);
-	}
+    if (typeof exports !== 'undefined') {
+        module.exports = factory();
+    } else {
+        window.LightRouter = factory(window);
+    }
 
-}(typeof window === 'undefined' ? undefined : window, function(window) {
+}(function(window) {
 
-	function LightRouter(options)
-	{
-		/**
-		 * Path root (will be stripped out when testing path-based routes)
-		 * @type string
-		 */
-		this.pathRoot = '';
+    function LightRouter(options) {
+        /**
+         * Path root (will be stripped out when testing path-based routes)
+         * @type string
+         */
+        this.pathRoot = '';
 
-		/**
-		 * Routes
-		 * @type array
-		 */
-		this.routes = [];
+        /**
+         * Routes
+         * @type array
+         */
+        this.routes = [];
 
-		/**
-		 * Default routing type [hash or path]
-		 * @type string
-		 */
-		this.type = 'path';
+        /**
+         * Default routing type [hash or path]
+         * @type string
+         */
+        this.type = 'path';
 
-		/**
-		 * Custom path (mainly used for testing)
-		 * @type string
-		 */
-		this.path = null;
+        /**
+         * Custom path (mainly used for testing)
+         * @type string
+         */
+        this.path = null;
 
-		/**
-		 * Custom hash (mainly used for testing)
-		 * @type string
-		 */
-		this.hash = null;
+        /**
+         * Custom hash (mainly used for testing)
+         * @type string
+         */
+        this.hash = null;
 
-		/**
-		 * Context to call matched routes under
-		 * @type {mixed}
-		 */
-		this.context = this;
+        /**
+         * Context to call matched routes under
+         * @type {mixed}
+         */
+        this.context = this;
 
-		/**
-		 * Handler for string based callbacks
-		 * @type {object|function}
-		 */
-		this.handler = window;
+        /**
+         * Handler for string based callbacks
+         * @type {object|function}
+         */
+        this.handler = window;
 
-		/**
-		 * Named param replace and matching regex
-		 * @type {Object}
-		 */
-		var namedParam = '([\\w-]+)';
-		this.namedParam = {
-			match: new RegExp('{(' + namedParam + ')}', 'g'),
-			replace: namedParam
-		};
+        /**
+         * Named param replace and matching regex
+         * @type {Object}
+         */
+        var namedParam = '([\\w-]+)';
+        this.namedParam = {
+            match: new RegExp('{(' + namedParam + ')}', 'g'),
+            replace: namedParam
+        };
 
-		options = options || {};
+        options = options || {};
 
-		if (options.type)      this.setType(options.type);
-		if (options.path)      this.setPath(options.path);
-		if (options.pathRoot)  this.setPathRoot(options.pathRoot);
-		if (options.hash)      this.setHash(options.hash);
-		if (options.context)   this.setContext(options.context);
-		if (options.handler)   this.setHandler(options.handler);
+        if (options.type) this.setType(options.type);
+        if (options.path) this.setPath(options.path);
+        if (options.pathRoot) this.setPathRoot(options.pathRoot);
+        if (options.hash) this.setHash(options.hash);
+        if (options.context) this.setContext(options.context);
+        if (options.handler) this.setHandler(options.handler);
 
-		if (options.routes)
-		{
-			var route;
-			for (route in options.routes)
-			{
-				this.add(route, options.routes[route]);
-			}
-		}
-	}
+        if (options.routes) {
+            var route;
+            for (route in options.routes) {
+                this.add(route, options.routes[route]);
+            }
+        }
+    }
 
-	LightRouter.prototype = {
+    LightRouter.prototype = {
 
-		/**
-		 * Route constructor
-		 * @type {Route}
-		 */
-		Route: Route,
+        /**
+         * Route constructor
+         * @type {Route}
+         */
+        Route: Route,
 
-		/**
-		 * Add a route
-		 * @param string|RegExp   route
-		 * @param string|function callback
-		 * @return self
-		 */
-		add: function(route, callback) {
-			this.routes.push(new this.Route(route, callback, this));
-			return this;
-		},
+        /**
+         * Add a route
+         * @param string|RegExp   route
+         * @param string|function callback
+         * @return self
+         */
+        add: function(route, callback) {
+            this.routes.push(new this.Route(route, callback, this));
+            return this;
+        },
 
 
-		/**
-		 * Empty/clear all the routes
-		 * @return self
-		 */
-		empty: function() {
-			this.routes = [];
-			return this;
-		},
+        /**
+         * Empty/clear all the routes
+         * @return self
+         */
+        empty: function() {
+            this.routes = [];
+            return this;
+        },
 
-		/**
-		 * Set's the routing type
-		 * @param self
-		 */
-		setType: function(type) {
-			this.type = type;
-			return this;
-		},
+        /**
+         * Set's the routing type
+         * @param self
+         */
+        setType: function(type) {
+            this.type = type;
+            return this;
+        },
 
-		/**
-		 * Set the path root url
-		 * @param string url
-		 * @return self
-		 */
-		setPathRoot: function(url) {
-			this.pathRoot = url;
-			return this;
-		},
+        /**
+         * Set the path root url
+         * @param string url
+         * @return self
+         */
+        setPathRoot: function(url) {
+            this.pathRoot = url;
+            return this;
+        },
 
-		/**
-		 * Sets the custom path to test routes against
-		 * @param  string path
-		 * @return self
-		 */
-		setPath: function(path) {
-			this.path = path;
-			return this;
-		},
+        /**
+         * Sets the custom path to test routes against
+         * @param  string path
+         * @return self
+         */
+        setPath: function(path) {
+            this.path = path;
+            return this;
+        },
 
-		/**
-		 * Sets the custom hash to test routes against
-		 * @param  string hash
-		 * @return self
-		 */
-		setHash: function(hash) {
-			this.hash = hash;
-			return this;
-		},
+        /**
+         * Sets the custom hash to test routes against
+         * @param  string hash
+         * @return self
+         */
+        setHash: function(hash) {
+            this.hash = hash;
+            return this;
+        },
 
-		/**
-		 * Sets context to call matched routes under
-		 * @param  mixed context
-		 * @return self
-		 */
-		setContext: function(context) {
-			this.context = context;
-			return this;
-		},
+        /**
+         * Sets context to call matched routes under
+         * @param  mixed context
+         * @return self
+         */
+        setContext: function(context) {
+            this.context = context;
+            return this;
+        },
 
-		/**
-		 * Set handler
-		 * @param  mixed context
-		 * @return self
-		 */
-		setHandler: function(handler) {
-			this.handler = handler;
-			return this;
-		},
+        /**
+         * Set handler
+         * @param  mixed context
+         * @return self
+         */
+        setHandler: function(handler) {
+            this.handler = handler;
+            return this;
+        },
 
-		/**
-		 * Gets the url to test the routes against
-		 * @return self
-		 */
-		getUrl: function(routeType) {
+        /**
+         * Gets the url to test the routes against
+         * @return self
+         */
+        getUrl: function(routeType) {
 
-			var url;
-			routeType = routeType || this.type;
+            var url;
+            routeType = routeType || this.type;
 
-			if (routeType == 'path')
-			{
-				var rootRegex = new RegExp('^' + this.pathRoot + '/?');
-				url = this.path || window.location.pathname.substring(1);
-				url = url.replace(rootRegex, '');
-			}
-			else if (routeType == 'hash')
-			{
-				url = this.hash || window.location.hash.substring(1);
-			}
-				
-			return decodeURI(url);
-		},
+            if (routeType == 'path') {
+                var rootRegex = new RegExp('^' + this.pathRoot + '/?');
+                url = this.path || window.location.pathname.substring(1);
+                url = url.replace(rootRegex, '');
+            } else if (routeType == 'hash') {
+                url = this.hash || window.location.hash.substring(1);
+            }
 
-		/**
-		 * Attempt to match a one-time route and callback
-		 *
-		 * @param  {string} path
-		 * @param  {closure|string} callback
-		 * @return {mixed}
-		 */
-		match: function(path, callback) {
-			var route = new this.Route(path, callback, this);
-			if (route.test(this.getUrl()))
-			{
-				return route.run();
-			}
-		},
+            return decodeURI(url);
+        },
 
-		/**
-		 * Run the router
-		 * @return Route|undefined
-		 */
-		run: function() {
-			var url = this.getUrl(), route;
+        /**
+         * Attempt to match a one-time route and callback
+         *
+         * @param  {string} path
+         * @param  {closure|string} callback
+         * @return {mixed}
+         */
+        match: function(path, callback) {
+            var route = new this.Route(path, callback, this);
+            if (route.test(this.getUrl())) {
+                return route.run();
+            }
+        },
 
-			for (var i in this.routes)
-			{
-				// Get the route
-				route = this.routes[i];
+        bindHash: function() {
+            if (this.type === 'hash') {
+                var _this = this;
+                window.addEventListener('hashchange', function() {
+                    _this.runRoute();
+                }, false);
+            }
+        },
 
-				// Test and run the route if it matches
-				if (route.test(url))
-				{
-					route.run();
-					return route;
-				}
-			}
-		}
-	};
+        runRoute: function() {
+            var url = this.getUrl(),
+                route;
+
+            for (var i in this.routes) {
+                // Get the route
+                route = this.routes[i];
+
+                // Test and run the route if it matches
+                if (route.test(url)) {
+                    route.run();
+                    return route;
+                }
+            }
+        },
+
+        /**
+         * Run the router
+         * @return Route|undefined
+         */
+        run: function() {
+            this.bindHash();
+
+            return this.runRoute();
+        }
+    };
 
 
-	/**
-	 * Route object
-	 * @param {string} path
-	 * @param {string} closure
-	 * @param {LightRouter} router  Instance of the light router the route belongs to.
-	 */
-	function Route(path, callback, router)
-	{
-		this.path = path;
-		this.callback = callback;
-		this.router = router;
-		this.values = [];
-	}
+    /**
+     * Route object
+     * @param {string} path
+     * @param {string} closure
+     * @param {LightRouter} router  Instance of the light router the route belongs to.
+     */
+    function Route(path, callback, router) {
+        this.path = path;
+        this.callback = callback;
+        this.router = router;
+        this.values = [];
+    }
 
-	Route.prototype = {
+    Route.prototype = {
 
-		/**
-		 * Converts route to a regex (if required) so that it's suitable for matching against.
-		 * @param  string route
-		 * @return RegExp
-		 */
-		regex: function() {
+        /**
+         * Converts route to a regex (if required) so that it's suitable for matching against.
+         * @param  string route
+         * @return RegExp
+         */
+        regex: function() {
 
-			var path = this.path;
+            var path = this.path;
 
-			if (typeof path === 'string')
-			{
-				return new RegExp('^' + path.replace(/\//g, '\\/').replace(this.router.namedParam.match, this.router.namedParam.replace) + '$');
-			}
-			return path;
-		},
+            if (typeof path === 'string') {
+                return new RegExp('^' + path.replace(/\//g, '\\/').replace(this.router.namedParam.match, this.router.namedParam.replace) + '$');
+            }
+            return path;
+        },
 
-		/**
-		 * Get the matching param keys
-		 * @return object  Object keyed with param name (or index) with the value.
-		 */
-		params: function() {
+        /**
+         * Get the matching param keys
+         * @return object  Object keyed with param name (or index) with the value.
+         */
+        params: function() {
 
-			var obj = {}, name, values = this.values, params = values, i, t = 0, path = this.path;
+            var obj = {},
+                name, values = this.values,
+                params = values,
+                i, t = 0,
+                path = this.path;
 
-			if (typeof path === 'string')
-			{
-				t = 1;
-				params = path.match(this.router.namedParam.match);
-			}
-			
-			for (i in params)
-			{
-				name = t ? params[i].replace(this.router.namedParam.match, '$1') : i;
-				obj[name] = values[i];
-			}
+            if (typeof path === 'string') {
+                t = 1;
+                params = path.match(this.router.namedParam.match);
+            }
 
-			return obj;
-		},
+            for (i in params) {
+                name = t ? params[i].replace(this.router.namedParam.match, '$1') : i;
+                obj[name] = values[i];
+            }
 
-		/**
-		 * Test the route to see if it matches
-		 * @param  {string} url Url to match against
-		 * @return {boolean}
-		 */
-		test: function(url) {
-			var matches;
-			if (matches = url.match(this.regex()))
-			{
-				this.values = matches.slice(1);
-				return true;
-			}
-			return false;
-		},
+            return obj;
+        },
 
-		/**
-		 * Run the route callback with the matched params
-		 * @return {mixed}
-		 */
-		run: function() {
-			if (typeof this.callback === 'string')
-			{
-				return this.router.handler[this.callback](this.params());
-			}
-			return this.callback.apply(this.router.context, [this.params()]);
-		}
-	};
+        /**
+         * Test the route to see if it matches
+         * @param  {string} url Url to match against
+         * @return {boolean}
+         */
+        test: function(url) {
+            var matches;
+            if (matches = url.match(this.regex())) {
+                this.values = matches.slice(1);
+                return true;
+            }
+            return false;
+        },
 
-	return LightRouter;
+        /**
+         * Run the route callback with the matched params
+         * @return {mixed}
+         */
+        run: function() {
+            if (typeof this.callback === 'string') {
+                return this.router.handler[this.callback](this.params());
+            }
+            return this.callback.apply(this.router.context, [this.params()]);
+        }
+    };
+
+    return LightRouter;
 
 }));
